144e3a77d446fccfe891f8f2df50914f286d8954
yamaguchi0707 <o9o9o152463@docomo.ne.jp>
2026-07-24 02:49:39 +0900

    Create player_data.py

---
+++ player_data.py
@@ -0,0 +1,71 @@
+def get_players():
+
+    players = [
+
+        {
+
+            "boat": 1,
+
+            "number": 0000,
+
+            "name": "選手名",
+
+            "rank": "A1",
+
+            "age": 0,
+
+            "term": 0,
+
+            "area": "",
+
+            "win_rate": 0,
+
+            "second_rate": 0,
+
+            "third_rate": 0,
+
+            "st": 0.00,
+
+            "motor_rate": 0,
+
+            "exhibition_time": 0.00,
+
+            "average_exhibition_time": 0.00
+
+        },
+
+        {
+
+            "boat": 2,
+
+            "number": 0000,
+
+            "name": "選手名",
+
+            "rank": "A1",
+
+            "age": 0,
+
+            "term": 0,
+
+            "area": "",
+
+            "win_rate": 0,
+
+            "second_rate": 0,
+
+            "third_rate": 0,
+
+            "st": 0.00,
+
+            "motor_rate": 0,
+
+            "exhibition_time": 0.00,
+
+            "average_exhibition_time": 0.00
+
+        }
+
+    ]
+
+    return players
