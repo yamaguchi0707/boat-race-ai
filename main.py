5a7f2ed87d65dc21dc36c6e087eda383ed803cbe
yamaguchi0707 <o9o9o152463@docomo.ne.jp>
2026-07-23 21:39:46 +0900

    Refactor print statements for better clarity

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+
 from predict import make_prediction
 
-print("=== ボートレースAI 起動 ===")
+print("=== ボートレースAI起動 ===")
 
 result = make_prediction()
 
-print(result)
+print("\n■選手評価")
 
-print("=== 解析終了 ===")
+for player in result["順位評価"]:
+
+    print(
+
+        f"{player['boat']}号艇 : {player['score']:.1f}点"
+
+    )
+
+print("\n■予想")
+
+print("本命:", result["本命"])
+
+print("中穴:", result["中穴"])
+
+print("穴:", result["穴"])
+
+print("\n=== 解析終了 ===")
