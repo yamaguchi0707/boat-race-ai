e574ac33a1373ce65515115970c0b871166f349a
yamaguchi0707 <o9o9o152463@docomo.ne.jp>
2026-07-23 22:04:57 +0900

    Create data.py

---
+++ data.py
@@ -0,0 +1,153 @@
+def get_race_data():
+
+    race = {
+
+        "venue": "未設定",
+
+        "race_number": 1,
+
+        "weather": {
+
+            "wind": 0,
+
+            "direction": "なし",
+
+            "temperature": 25
+
+        },
+
+        "water": {
+
+            "type": "汽水",
+
+            "condition": "良"
+
+        },
+
+        "players": [
+
+            {
+
+                "boat": 1,
+
+                "name": "選手1",
+
+                "rank": "A1",
+
+                "win_rate": 0,
+
+                "second_rate": 0,
+
+                "third_rate": 0,
+
+                "st": 0,
+
+                "motor_rate": 0
+
+            },
+
+            {
+
+                "boat": 2,
+
+                "name": "選手2",
+
+                "rank": "A2",
+
+                "win_rate": 0,
+
+                "second_rate": 0,
+
+                "third_rate": 0,
+
+                "st": 0,
+
+                "motor_rate": 0
+
+            },
+
+            {
+
+                "boat": 3,
+
+                "name": "選手3",
+
+                "rank": "B1",
+
+                "win_rate": 0,
+
+                "second_rate": 0,
+
+                "third_rate": 0,
+
+                "st": 0,
+
+                "motor_rate": 0
+
+            },
+
+            {
+
+                "boat": 4,
+
+                "name": "選手4",
+
+                "rank": "B1",
+
+                "win_rate": 0,
+
+                "second_rate": 0,
+
+                "third_rate": 0,
+
+                "st": 0,
+
+                "motor_rate": 0
+
+            },
+
+            {
+
+                "boat": 5,
+
+                "name": "選手5",
+
+                "rank": "B1",
+
+                "win_rate": 0,
+
+                "second_rate": 0,
+
+                "third_rate": 0,
+
+                "st": 0,
+
+                "motor_rate": 0
+
+            },
+
+            {
+
+                "boat": 6,
+
+                "name": "選手6",
+
+                "rank": "B1",
+
+                "win_rate": 0,
+
+                "second_rate": 0,
+
+                "third_rate": 0,
+
+                "st": 0,
+
+                "motor_rate": 0
+
+            }
+
+        ]
+
+    }
+
+    return race
